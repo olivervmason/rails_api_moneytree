2e42959335dd067c018b5f46921b2706128566b4
Oliver Mason <omason@protonmail.com>
2020-07-20 14:44:36 +1000

    User model now has_many:columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :columns
 
     def to_token_payload
         {
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_041515) do
+ActiveRecord::Schema.define(version: 2020_07_20_044301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "columns", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_columns_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_07_20_041515) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "columns", "users"
 end
