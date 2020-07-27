900dbe6985661ff709ca373ec2055b1ef263e92c
Oliver Mason <omason@protonmail.com>
2020-07-27 12:43:32 +1000

    add config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) } to knock.rb

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -1,5 +1,7 @@
 Knock.setup do |config|
 
+  config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) }
+
   ## Expiration claim
   ## ----------------
   ##
@@ -36,7 +38,7 @@ Knock.setup do |config|
   ## Configure the key used to sign tokens.
   ##
   ## Default:
-  config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
+  # config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
 
   ## If using Auth0, uncomment the line below
   # config.token_secret_signature_key = -> { JWT.base64url_decode Rails.application.secrets.auth0_client_secret }
